fd7cec46988d721ebb04b2251537baf46036fccb
R Panos <randompkp@yahoo.com>
2014-08-16 19:55:18 -0700

    my first jasmine test
    And ignoring node modules now.  Install karma cmd line in addition!

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -2,13 +2,53 @@
 
 /* jasmine specs for services go here */
 
-describe('service', function() {
-  beforeEach(module('myApp.services'));
+describe('service', function () {
+    beforeEach(module('myApp.services'));
 
 
-  describe('version', function() {
-    it('should return current version', inject(function(version) {
-      expect(version).toEqual('0.1');
-    }));
-  });
+    describe('version', function () {
+
+        it('should return current version', inject(function (version) {
+            expect(version).toEqual('0.1');
+        }));
+
+    });
+
+
+    describe('arcCalcService test', function(){
+        describe('when I call arcCalcService.guide', function(){
+            it('returns length: 400', function(){
+                var $injector = angular.injector([ 'myApp' ]);
+                var myService = $injector.get( 'arcCalcService' );
+                expect( myService.guide ).toEqual({length: 400});
+            })
+
+        });
+
+    });
+
+//
+//    describe('arcCalcService', function () {
+//
+//        it('should return good things', inject(function ($service) {
+//
+//            var arcServ = $service('arcCalcService', {$scope:scope});
+//
+//        }));
+//
+//    });
+//
+//    describe('PhoneListCtrl', function(){
+//
+//        beforeEach(module('phonecatApp'));
+//
+//        it('should create "phones" model with 3 phones', inject(function($controller) {
+//            var scope = {},
+//                ctrl = $controller('PhoneListCtrl', {$scope:scope});
+//
+//            expect(scope.phones.length).toBe(3);
+//        }));
+//
+//    });
+
 });
